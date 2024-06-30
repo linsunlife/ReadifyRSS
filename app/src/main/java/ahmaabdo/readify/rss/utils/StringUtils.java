8271f24be8475ded0d3fcc29ef61bbc0b1db46bd
linsunlife <linsunlife@gmail.com>
2024-06-30 21:52:22 +0800

    Format the code

--- app/src/main/java/ahmaabdo/readify/rss/utils/StringUtils.java
+++ app/src/main/java/ahmaabdo/readify/rss/utils/StringUtils.java
@@ -20,50 +20,32 @@
 
 package ahmaabdo.readify.rss.utils;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import ahmaabdo.readify.rss.MainApplication;
 
-@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class StringUtils {
 
-    private static final DateFormat TIME_FORMAT = android.text.format.DateFormat.getTimeFormat(MainApplication.getContext());
-    private static final int SIX_HOURS = 21600000; // six hours in milliseconds
-    private static DateFormat DATE_SHORT_FORMAT = null;
-
-    static {
-        // getBestTimePattern() is only available in API 18 and up (Android 4.3 and better)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            DATE_SHORT_FORMAT = new SimpleDateFormat(android.text.format.DateFormat.getBestDateTimePattern(MainApplication.getContext().getResources().getConfiguration().locale, "d MMM"));
-        } else {
-            DATE_SHORT_FORMAT = android.text.format.DateFormat.getDateFormat(MainApplication.getContext());
-        }
-    }
-
     static public String getDateTimeString(long timestamp) {
-        String outString;
-
-        Date date = new Date(timestamp);
         Calendar calTimestamp = Calendar.getInstance();
         calTimestamp.setTimeInMillis(timestamp);
         Calendar calCurrent = Calendar.getInstance();
-
-        if (calCurrent.getTimeInMillis() - timestamp < SIX_HOURS || calCurrent.get(Calendar.DAY_OF_MONTH) == calTimestamp.get(Calendar.DAY_OF_MONTH)) {
-            outString = TIME_FORMAT.format(date);
+        Locale locale = MainApplication.getContext().getResources().getConfiguration().locale;
+        String format;
+        if (calCurrent.get(Calendar.YEAR) != calTimestamp.get(Calendar.YEAR)) {
+            format = android.text.format.DateFormat.getBestDateTimePattern(locale, "d MMM yyyy HH:mm");
+        } else if (calCurrent.get(Calendar.MONTH) != calTimestamp.get(Calendar.MONTH) || calCurrent.get(Calendar.DAY_OF_MONTH) != calTimestamp.get(Calendar.DAY_OF_MONTH)) {
+            format = android.text.format.DateFormat.getBestDateTimePattern(locale, "d MMM HH:mm");
         } else {
-            outString = DATE_SHORT_FORMAT.format(date) + ' ' + TIME_FORMAT.format(date);
+            format = "HH:mm";
         }
-
-        return outString;
+        return new SimpleDateFormat(format, locale).format(new Date(timestamp));
     }
 
     public static String getMd5(String input) {
